e3a4cbe4c998ea2959a83e4303890218199238f6
FreakeyPlays <chrismerck@gmx.de>
2022-07-21 19:14:32 +0200

    style(navbar): adjusted responsiveness

--- components/navbar.js
+++ components/navbar.js
@@ -16,7 +16,7 @@ const SocialIconLink = ( {Component, uri, hover} ) => {
 
 const NavItem = ( {link} ) => {
     return(
-        <div className="w-24" data-section={link}>
+        <div className="w-24 lg:text-center" data-section={link}>
             <Link href={"#" + link}>
                 <a className="text-white font-bold relative uppercase before:content-[''] before:absolute
                                 before:w-full before:h-1 before:bg-l-primary before:dark:bg-d-primary 
@@ -42,12 +42,12 @@ const NavBar = () => {
             <div className="w-24 xl:w-[90vw] h-[80vh] xl:h-24 max-h-[calc(1080px-256px)] fixed rounded-xl rainbow_gradient opacity-40 filter blur-3xl 
                             right-0 xl:right-1/2 bottom-1/2 xl:bottom-0 translate-y-[50%] xl:translate-y-[-50%] xl:translate-x-[50%] mr-8">
             </div>
-            <nav className="w-32 xl:w-[90vw] h-[80vh] xl:h-24 max-h-[calc(1080px-256px)] fixed flex flex-col xl:flex-row right-0 xl:right-1/2 
+            <nav className="w-32 xl:w-[90vw] h-[80vh] xl:h-24 lg:h-16 max-h-[calc(1080px-256px)] fixed flex flex-col xl:flex-row right-0 xl:right-1/2 
                             xl:translate-x-[50%] bottom-1/2 xl:bottom-0 translate-y-[50%] xl:translate-y-0 mr-8 xl:mr-0 xl:mb-8 rounded-2xl
                             bg-gradient-to-br from-[#ffffff26] to-[#ffffff0d] border-[1px] border-[#ffffff2e] border-solid bg-clip-padding 
-                            backdrop-filter backdrop-blur-xl shadow-md outline outline-2 outline-white outline-offset-8"
+                            backdrop-filter backdrop-blur-xl shadow-md outline outline-2 outline-white outline-offset-8 z-50"
             >
-                <div className="w-32 xl:w-24 h-32 xl:h-24 flex flex-col justify-center items-center">
+                <div className="w-32 xl:w-24 h-32 xl:h-24 flex flex-col justify-center items-center lg:hidden">
                     <Link href="/">
                         <a className="w-24 xl:w-16 h-24 xl:h-16 p-1 rounded-full border-4 border-solid border-white drop-shadow-xl 
                                       hover:border-l-primary focus:border-l-primary dark:hover:border-d-primary dark:focus:border-d-primary">
@@ -62,8 +62,8 @@ const NavBar = () => {
                     </Link>
                 </div>
 
-                <div className="w-32 xl:w-[calc(100%-192px)] lg:w-[calc(100%-96px)]  h-[calc(100%-256px)] xl:h-full flex flex-col justify-center
-                                text-xl xl:text-2xl lg:text-4xl">
+                <div className="w-32 xl:w-[calc(100%-192px)] lg:w-full h-[calc(100%-256px)] xl:h-full flex flex-col justify-center
+                                text-xl xl:text-2xl lg:text-xl">
                     <div id="nav_li_container" className="w-full h-1/2 flex flex-col xl:flex-row items-center justify-between xl:justify-around">
                         <NavItem 
                             link="about" 
