06f6eb31b65436e1a4095991b5441c69c9c09e4b
Christoph Merck <78086475+FreakeyPlays@users.noreply.github.com>
2025-04-17 12:10:09 +0200

    feat: add fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 // @ts-check
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import icon from 'astro-icon';
 
@@ -9,6 +9,21 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://chrismerck.dev',
+
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: 'Montserrat',
+        cssVariable: '--font-serif',
+      },
+      {
+        provider: fontProviders.google(),
+        name: 'Raleway',
+        cssVariable: '--font-sans',
+      }
+    ],
+  },
 
   vite: {
     plugins: [tailwindcss()],
