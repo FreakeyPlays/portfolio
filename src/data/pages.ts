477b859865a0030e4c18f5c27902411a725463df
Christoph Merck <78086475+FreakeyPlays@users.noreply.github.com>
2025-04-28 15:06:10 +0200

    chore: extract pages to separate file

---
+++ src/data/pages.ts
@@ -0,0 +1,13 @@
+export type Page = {
+  label: string,
+  url: string,
+  samePage: boolean,
+};
+
+export const pages: Page[] = [
+  { label: 'about', url: '#about', samePage: true },
+  { label: 'skills', url: '#skills', samePage: true },
+  { label: 'career', url: '#career', samePage: true },
+  { label: 'education', url: '#education', samePage: true },
+  { label: 'projects', url: '#projects', samePage: true },
+];
