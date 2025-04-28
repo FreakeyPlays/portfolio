ff32727be1a116a034a8e591edc082a076c22991
Christoph Merck <78086475+FreakeyPlays@users.noreply.github.com>
2025-04-28 15:05:35 +0200

    chore: update Social Links

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -1,40 +1,35 @@
+import BlueskyIcon from '../assets/icons/social/bluesky.svg';
+import GitHubIcon from '../assets/icons/social/github.svg';
+import LinkedInIcon from '../assets/icons/social/linkedin.svg';
+
 export type SocialLink = {
-  me?: string;
   /** Longer descriptive label, e.g. `"Join the Astro community on Discord"` */
   text: string;
   /** Short label with the name of the platform, e.g. `"Discord"`*/
   label: string;
-  icon: string;
+  icon: ((_props: astroHTML.JSX.SVGAttributes) => any) & ImageMetadata;
   href: string;
-  /** Platform ID, e.g. `"discord"` */
-  platform: string;
   footerOnly?: boolean;
 };
 
 const socialLinks: SocialLink[] = [
   {
-    platform: 'bluesky',
-    icon: '',
-    me: 'https://bsky.app/profile/freakyplays.bsky.social',
-    label: 'Bluesky',
-    text: 'Follow me on Bluesky',
-    href: 'https://bsky.app/profile/freakyplays.bsky.social',
-  },
-  {
-    platform: 'github',
-    icon: '',
-    me: 'https://github.com/FreakeyPlays',
+    icon: GitHubIcon,
     label: 'GitHub',
     text: 'See my GitHub Projects',
     href: 'https://github.com/FreakeyPlays',
   },
   {
-    platform: 'linkedin',
-    icon: '',
-    me: 'https://www.linkedin.com/in/christoph-merck',
+    icon: LinkedInIcon,
     label: 'LinkedIn',
     text: 'Follow me on LinkedIn',
     href: 'https://www.linkedin.com/in/christoph-merck',
+  },
+  {
+    icon: BlueskyIcon,
+    label: 'Bluesky',
+    text: 'Follow me on Bluesky',
+    href: 'https://bsky.app/profile/freakyplays.bsky.social',
   },
 ];
 
