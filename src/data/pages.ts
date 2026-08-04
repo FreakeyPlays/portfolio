4be113284b9486bbf52804f8ea91a0a9dc654547
Christoph Merck <78086475+FreakeyPlays@users.noreply.github.com>
2026-08-04 11:47:22 +0200

    fix: navigation with ancors works on all pages now

--- src/data/pages.ts
+++ src/data/pages.ts
@@ -5,9 +5,9 @@ export type Page = {
 };
 
 export const pages: Page[] = [
-  { label: 'about', url: '#about', samePage: true },
-  { label: 'skills', url: '#skills', samePage: true },
-  { label: 'career', url: '#career', samePage: true },
-  { label: 'education', url: '#education', samePage: true },
-  { label: 'projects', url: '#projects', samePage: true },
+  { label: 'about', url: '/#about', samePage: true },
+  { label: 'skills', url: '/#skills', samePage: true },
+  { label: 'career', url: '/#career', samePage: true },
+  { label: 'education', url: '/#education', samePage: true },
+  { label: 'projects', url: '/#projects', samePage: true },
 ];
