a024adda5a4c77186802be8d0f83e500a7719b03
Christoph Merck <78086475+FreakeyPlays@users.noreply.github.com>
2026-08-09 14:46:30 +0200

    fix: remove / from paht so page does not reload

--- src/data/pages.ts
+++ src/data/pages.ts
@@ -6,8 +6,8 @@ export type Page = {
 
 export const pages: Page[] = [
   { label: 'about', url: '/', samePage: true },
-  { label: 'skills', url: '/#skills', samePage: true },
-  { label: 'career', url: '/#career', samePage: true },
-  { label: 'education', url: '/#education', samePage: true },
-  { label: 'projects', url: '/#projects', samePage: true },
+  { label: 'skills', url: '#skills', samePage: true },
+  { label: 'career', url: '#career', samePage: true },
+  { label: 'education', url: '#education', samePage: true },
+  { label: 'projects', url: '#projects', samePage: true },
 ];
