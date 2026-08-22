7db95eba83f7c65258abaf9ffebd657d0607c07b
Christoph Merck <78086475+FreakeyPlays@users.noreply.github.com>
2026-08-22 07:27:25 +0200

    fix: category should be singe not array

--- src/content.config.ts
+++ src/content.config.ts
@@ -28,7 +28,7 @@ const posts = defineCollection({
       intro: z.string(),
       faq: z.array(z.object({ question: z.string(), answer: z.string() })).default([]),
       tags: z.array(z.string()).default([]),
-      categorys: z.array(z.string()).optional(),
+      categorys: z.string(),
       customSlug: z.string().optional(),
       isPublished: z.boolean().default(false),
       createdAt: z.coerce.date(),
