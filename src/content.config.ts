a2d5b815d17cf89603527751e82c7bc5d4251868
Christoph Merck <78086475+FreakeyPlays@users.noreply.github.com>
2026-08-23 21:09:59 +0200

    feat: add slug to posts and enforce naming convention

--- src/content.config.ts
+++ src/content.config.ts
@@ -23,8 +23,10 @@ const posts = defineCollection({
   loader: glob({ pattern: '**/*.mdx', base: './src/content/posts' }),
   schema: ({ image }) =>
     z.object({
+      order: z.number().optional(),
       image: image(),
       title: z.string(),
+      slug: z.string(),
       intro: z.string(),
       faq: z.array(z.object({ question: z.string(), answer: z.string() })).default([]),
       tags: z.array(z.string()).default([]),
