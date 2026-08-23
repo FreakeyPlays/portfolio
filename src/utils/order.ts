9ba9e70b1fdf8bf94c2cea6d55b81851819cab1f
Christoph Merck <78086475+FreakeyPlays@users.noreply.github.com>
2026-08-23 21:26:17 +0200

    feat: use inverse byOrder sorting

--- src/utils/order.ts
+++ src/utils/order.ts
@@ -1,2 +1,5 @@
 export const byOrder = <T extends { data: { order?: number } }>(a: T, b: T) =>
   (a.data.order ?? Number.MAX_SAFE_INTEGER) - (b.data.order ?? Number.MAX_SAFE_INTEGER);
+
+export const byOrderInverse = <T extends { data: { order?: number } }>(a: T, b: T) =>
+  (b.data.order ?? Number.MAX_SAFE_INTEGER) - (a.data.order ?? Number.MAX_SAFE_INTEGER);
