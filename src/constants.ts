51516785eb1b6402c4044a919bb486b353eedb73
Christoph Merck <78086475+FreakeyPlays@users.noreply.github.com>
2026-08-28 17:32:12 +0200

    feat: use CloudFlare to detect prod

--- src/constants.ts
+++ src/constants.ts
@@ -2,3 +2,6 @@ export const URL_MAPPINGS: Record<string, string> = {
   '/': 'Home',
   '/blog': 'Blog',
 };
+
+export const isProduction =
+  process.env.CF_PAGES !== '1' || process.env.CF_PAGES_BRANCH === 'master';
